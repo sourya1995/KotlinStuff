53b40305c5b23462c69f12f197c6a6d9704580cd
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-09 11:30:09 +0530

    Adding user functionality

--- BankOps/src/main/kotlin/BankOps.kt
+++ BankOps/src/main/kotlin/BankOps.kt
@@ -65,5 +65,57 @@ class BankOps {
                 return deposit(amount)
             }
         }
+
+        fun transfer(mode: String){
+            val transferAmount: Int
+
+            when(mode) {
+                "withdraw" -> {
+                    if(accountType == "debit"){
+                        transferAmount = debitWithdraw(money)
+                    }else {
+                        transferAmount = withdraw(money)
+                    }
+
+                    println("The amount you withdrew is ${transferAmount} dollars.")
+                }
+                "deposit" -> {
+                    if (accountType == "credit"){
+                        transferAmount = creditDeposit(money)
+                    } else {
+                        transferAmount = deposit(money)
+                    }
+
+                    println("The amount you deposited is ${transferAmount} dollars.")
+                }
+
+                else -> return
+            }
+        }
+
+        var isSystemOpen = true
+        var option = 0
+
+        while (isSystemOpen == true){
+            println("What would you like to do?")
+            println("1. Check account balance")
+            println("2. Withdraw money")
+            println("3. Deposit money")
+            println("4. Close the system")
+            println("5. Which option do you choose? (1, 2, 3 or 4)")
+            option = (1..5).random()
+            println("You selected ${option}")
+
+            when (option){
+                1 -> println("The current balance is ${accountBalance} dollars.")
+                2 -> transfer("withdraw")
+                3 -> transfer("deposit")
+                4 -> {
+                    isSystemOpen = false
+                    println("The system is closed")
+                }
+                else -> continue
+            }
+        }
     }
 }
