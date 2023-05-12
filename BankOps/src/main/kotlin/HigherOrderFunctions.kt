97db84501827d96db38a66bb74e251664ef7ee96
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-12 18:21:07 +0530

    Adding Functional Programming

---
+++ BankOps/src/main/kotlin/HigherOrderFunctions.kt
@@ -0,0 +1,17 @@
+import kotlin.random.Random
+
+class HigherOrderFunctions {
+    fun main(){
+        var maximumDiscountValue = 0
+
+        repeat(3) {
+            index -> val discount = Random.nextInt(10)
+            println("Attempt ${index+1}: $discount")
+            if(discount > maximumDiscountValue){
+                maximumDiscountValue = discount
+            }
+        }
+
+        println(maximumDiscountValue)
+    }
+}
