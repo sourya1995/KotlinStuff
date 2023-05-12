3d611c930310dd849c3078281d432db601e08f27
Sourya Bhattacharya <37332817+sourya1995@users.noreply.github.com>
2023-05-12 15:48:24 +0530

    adding unit tests

---
+++ BankOps/src/main/kotlin/UnitTest.kt
@@ -0,0 +1,15 @@
+class UnitTest {
+    data class Product(
+        val title: String,
+        var price: Double,
+        var amount: Int
+    ) {
+        fun applyDiscount(discountPercent: Int){
+            if(amount > 0 && amount <= 5){
+                price -= (price * discountPercent / 100)
+            }
+        }
+    }
+
+
+}
---
+++ BankOps/src/test/testUnitTest.java
@@ -0,0 +1,25 @@
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+class testUnitTest{
+    @Test
+    fun applyDiscount_lessThanFive_discountApplied(){
+        val product = Product("Spaghetti", 20.00, 3)
+                product.applyDiscount(20)
+                        assertEquals(16.00, product.price, 0.0)
+    }
+
+    @Test
+    fun applyDiscount_moreThanFive_discountNotApplied(){
+        val product = Product("Steak", 30.00, 8)
+                product.applyDiscount(20)
+                        assertEquals(10.00, product.price, 0.0)
+    }
+
+    @Test
+    fun applyDiscount_outOfStock_discountNotApplied() {
+        val product = Product("Lasagna", 10.00, 0)
+                product.applyDiscount(20)
+                        assertEquals(10.00,product.price, 0.0)
+    }
+}
